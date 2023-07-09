ab53be92f5bd6e8d7c194c0f3ebbaa577451fa8d
JuicetinB <jabuonato@gmail.com>
2023-07-09 18:49:26 -0400

    trying to make package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='1.0',
    description='Exports clean versions of songs',
    author='JuicetinB',
-   author_email='jabuonato@gmail.com'
-   packages=find_packages(),
+   author_email='jabuonato@gmail.com',
+   packages=['cleansong'],
    install_requires=[], #external packages as dependencies 'demucs', 'whisper', 'pydub'
 )
